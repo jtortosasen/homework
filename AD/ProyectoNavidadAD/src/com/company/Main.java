161cdb915feb2d189dc9cd9c1d63aae70c6e43a9
Wasdf <jorge.tortosa@outlook.com>
2018-01-17 13:03:58 +0100

    progress

--- AD/ProyectoNavidadAD/src/com/company/Main.java
+++ AD/ProyectoNavidadAD/src/com/company/Main.java
@@ -3,45 +3,59 @@ package com.company;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     private static final String USER_DATABASE = "root";
     private static final String PWD_DATABASE = "admin";
     private static final String URL_CONN = "jdbc:mysql://localhost/";
 
+    private static boolean exit = false;
+
 
     public static void main(String[] args) {
-        // write your code here
+
         DataBaseManager dbManager = new DataBaseManager(USER_DATABASE, PWD_DATABASE, URL_CONN);
-        if(dbManager.connect()){
-            System.out.println("ha conectao");
+
+        if (dbManager.connect()) {
+            do {
+                Options.showMenu();
+
+                Scanner scanner = new Scanner(System.in);
+                int option = scanner.nextInt();
+                switch (option) {
+                    case 1: {
+                        Options.showMenu1();
+                        option = scanner.nextInt();
+                        Options.option1(option, dbManager);
+                        break;
+                    }
+
+                    case 2: {
+                        Options.showMenu2();
+                        option = scanner.nextInt();
+                        Options.options2(option, dbManager);
+                        break;
+                    }
+                    case 3:{
+                        Options.showMenu3();
+                        option = scanner.nextInt();
+                        Options.options3(option, dbManager);
+                        break;
+                    }
+                    case 4:{
+                        exitApplication();
+                    }
+                }
+            } while (!exit);
+        }
+        System.out.println("Cerrando acceso a DB");
+        if(dbManager.close()){
+            System.out.println("Cerrado.");
         }
     }
 
-    private static void showMenu(){
-        System.out.println("1. Departamentos");
-        System.out.println("2. Empleados");
-        System.out.println("3. Consultas");
+    private static void exitApplication(){
+        exit = true;
     }
-
-    private static void showMenu1(){
-        System.out.println("1. Crear departamento");
-        System.out.println("2. Modificar departamento");
-        System.out.println("3. Eliminar departmaento");
-        System.out.println("4. Buscar departamento");
-    }
-
-    private static void showMenu2(){
-        System.out.println("1. Crear empleado");
-        System.out.println("2. Modificar empleado");
-        System.out.println("3. Eliminar empleado");
-        System.out.println("4. Buscar empleado");
-    }
-
-    private static void showMenu3(){
-        System.out.println("1. Dado el código de un empleado ver su información");
-        System.out.println("2. Dado ");
-        System.out.println("3. Eliminar departmaento");
-        System.out.println("4. Buscar departamento");
-    }[]
 }
