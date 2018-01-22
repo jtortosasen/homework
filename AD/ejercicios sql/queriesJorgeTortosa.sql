d62267688cd86ae682737fa94c1e985b01a6f9be
Wasdf <jorge.tortosa@outlook.com>
2018-01-22 19:27:30 +0100

    fix spaces in comments

--- AD/ejercicios sql/queriesJorgeTortosa.sql
+++ AD/ejercicios sql/queriesJorgeTortosa.sql
@@ -1,10 +1,10 @@
---1
+-- 1
 create database tienda;
---2
+-- 2
 show databases;
---3
+-- 3
 use tienda;
---4
+-- 4
 create table 
 	FABRICANTES (Clave_fabricante int not null,
 				 Nombre varchar(30) not null,
@@ -16,11 +16,11 @@ create table
                  Clave_fabricante int,
                  primary key (Clave_articulo),
                  foreign key (Clave_fabricante) references FABRICANTES(Clave_fabricante));
---5
+-- 5
 show tables;
---6
+-- 6
 describe ARTICULOS;
---7
+-- 7
 insert into FABRICANTES(Clave_fabricante, nombre) values(1, 'kingston');
 insert into FABRICANTES(Clave_fabricante, nombre) values(2, 'Adata');
 insert into FABRICANTES(Clave_fabricante, nombre) values(3, 'Logitech');
@@ -37,61 +37,61 @@ insert into  ARTICULOS(Clave_articulo , nombre, precio, Clave_fabricante) values
 insert into  ARTICULOS(Clave_articulo , nombre, precio, Clave_fabricante) values(8, 'DVD Rom', 450, 2);
 insert into  ARTICULOS(Clave_articulo , nombre, precio, Clave_fabricante) values(9, 'CD Rom', 200, 2);
 insert into  ARTICULOS(Clave_articulo , nombre, precio, Clave_fabricante) values(10, 'Tarjeta de red', 180, 3);
---8
---a
+-- 8
+-- a
 select * from ARTICULOS;
---b
+-- b
 select Nombre from ARTICULOS;
---c
+-- c
 select Nombre, Precio from ARTICULOS;
---d
+-- d
 select distinct Nombre from ARTICULOS;
---e
+-- e
 select * from ARTICULOS where Clave_articulo = 5;
---f
+-- f
 select * from ARTICULOS where Nombre = 'Teclado';
---g
+-- g
 select * from ARTICULOS where Nombre = 'Memoria RAM' OR Nombre = 'Memoria USB';
---h
+-- h
 select * from ARTICULOS where Nombre LIKE 'M%';
---i
+-- i
 select * from ARTICULOS where Precio = 100;
---j
+-- j
 select * from ARTICULOS where Precio > 200;
---k
+-- k
 select * from ARTICULOS where Precio > 100 AND Precio < 350;
---l
+-- l 
 select avg(Precio) from ARTICULOS;
---m
+-- m
 select avg(Precio) from ARTICULOS where Clave_fabricante = 2;
---n
+-- n
 select Nombre, Precio from ARTICULOS ORDER BY(Nombre);
---o
+-- o
 select * from ARTICULOS ORDER BY(Precio)desc;
---p
+-- p
 select Nombre, Precio from ARTICULOS where Precio > 250 order by Precio desc, Nombre asc;
---q
+-- q
 select Clave_articulo, ARTICULOS.Nombre, Precio, FABRICANTES.Clave_fabricante, FABRICANTES.Nombre from ARTICULOS
 join FABRICANTES on ARTICULOS.Clave_fabricante = FABRICANTES.Clave_fabricante;
---r
+-- r
 select Clave_articulo, ARTICULOS.Nombre, FABRICANTES.Nombre from ARTICULOS
 join FABRICANTES on ARTICULOS.Clave_fabricante = FABRICANTES.Clave_fabricante
 Where ARTICULOS.Precio is not null;
---s
+-- s
 select ARTICULOS.Nombre, ARTICULOS.Precio from ARTICULOS
 join FABRICANTES on ARTICULOS.Clave_fabricante = FABRICANTES.Clave_fabricante
 where FABRICANTES.Nombre = 'Logitech' order by ARTICULOS.Nombre;
---t
+-- t
 select ARTICULOS.Nombre, ARTICULOS.Precio, FABRICANTES.Nombre from ARTICULOS
 join FABRICANTES on ARTICULOS.Clave_fabricante = FABRICANTES.Clave_fabricante
 Where FABRICANTES.Nombre = 'Lexar' OR FABRICANTES.Nombre = 'Kingston' order by ARTICULOS.Precio desc;
---u
+-- u
 insert into ARTICULOS(Clave_articulo, Nombre, Precio, Clave_fabricante) values(11, 'Altavoces', 120, 2);
---v
+-- v
 update ARTICULOS set ARTICULOS.Nombre = 'Impresora Laser' where ARTICULOS.Clave_articulo = 6;
---w
+-- w
 update ARTICULOS set ARTICULOS.Precio = (ARTICULOS.Precio - (ARTICULOS.Precio/100)*10) where ARTICULOS.Precio > 300;
---x
+-- x
 update ARTICULOS set ARTICULOS.Precio = (ARTICULOS.Precio - 10) where ARTICULOS.Precio > 300;
---y
+-- y
 delete from ARTICULOS where ARTICULOS.Clave_articulo = 6;
