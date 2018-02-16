7cf077e450fe075f251f6389b57ab34b62ad85ad
Wasdf <jorge.tortosa@outlook.com>
2018-02-16 14:05:14 +0100

    99% communication ring

---
+++ PSP/CommunicationRing/src/com/wasdf/ListenerToken.java
@@ -0,0 +1,54 @@
+package com.wasdf;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+
+public class ListenerToken extends Thread {
+
+    private int ringPosition;
+    private int ringLength;
+
+    private int portListener;
+
+    ServerUDP serverUDP;
+
+
+
+    public ListenerToken(int ringPosition, int ringLength, int portListener, ServerUDP serverUDP) {
+        this.ringPosition = ringPosition;
+        this.portListener = portListener;
+        this.serverUDP = serverUDP;
+        this.ringLength = ringLength;
+        System.out.println("Soy " + ringPosition + " y escucho en : " + portListener);
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                System.out.println("entrando en thread");
+                DatagramSocket socket = new DatagramSocket(portListener);
+                byte[] string = new byte[1000];
+                DatagramPacket messagePacket = new DatagramPacket(string, string.length);
+                socket.receive(messagePacket);
+                System.out.println("mensaje recibido");
+                String message = new String(messagePacket.getData(), 0, messagePacket.getLength());
+                socket.close();
+                if(message.contains("TOKEN")){
+                    System.out.println("Token recibido");
+                    serverUDP.setToken(message);
+                }else{
+                    if (Integer.parseInt(message) == ringLength) {
+                        System.out.println("Recibido último ");
+                        serverUDP.sendToken((portListener - ringLength) + 1);
+                    } else {
+                        System.out.println("Voy a enviar el token a: " + ((portListener - ringPosition) + Integer.parseInt(message)));
+                        serverUDP.sendToken((portListener - ringPosition) + Integer.parseInt(message));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- PSP/CommunicationRing/src/com/wasdf/ServerUDP.java
+++ PSP/CommunicationRing/src/com/wasdf/ServerUDP.java
@@ -1,29 +1,88 @@
 package com.wasdf;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
 public class ServerUDP {
 
-    private int ringLength;
+    private String messagePost;
+    private String tokenName;
+    private int portListener;
     private int ringPosition;
 
-    private String message;
-    private int portListener;
+    private ListenerToken listenerToken;
 
+    private int ringLength;
 
-
-    public ServerUDP(String arg, String arg1) {
-        ringLength = Integer.parseInt(arg);
-        ringPosition = Integer.parseInt(arg1);
-        checkStatus();
+    public void setToken(String tokenName){
+        this.tokenName = tokenName + "_" + ringPosition;
+        System.out.println(tokenName);
     }
 
-    private void checkStatus(){
-        if(ringPosition == 0){
-            message = "TOKEN";
-            port = 10000 + Integer.parseInt(ringPosition)
-        }else if(ringPosition == ringLength - 1){
+    public ServerUDP(String ringLength, String ringPosition) {
+        portListener = 10000;
+        tokenName = "TOKEN";
+        this.ringLength = Integer.parseInt(ringLength);
+        this.ringPosition = Integer.parseInt(ringPosition);
+        checkStatus(Integer.parseInt(ringPosition));
+    }
 
-        }else{
+    private void checkStatus(int ringPosition) {
+        if (ringPosition == 1) {
+            portListener += ringPosition;
+            listenerToken = new ListenerToken(ringPosition,ringLength, portListener, this);
+            listenerToken.start();
+        } else {
+            System.out.println("entrando en el else");
+            portListener += ringPosition;
+            messagePost = String.valueOf(ringPosition);
+            listenerToken = new ListenerToken(ringPosition, ringLength, portListener, this);
+            listenerToken.start();
+            getToken();
+        }
+    }
 
+    private void getToken() {
+        try {
+            DatagramSocket sSocket = new DatagramSocket();
+            InetAddress machineAddress = InetAddress.getByName("localhost");
+            byte[] string = messagePost.getBytes();
+            int portToSend = portListener -1;
+            if(ringPosition == ringLength){
+                portToSend = (portListener - ringLength) + 1 ;
+            }
+            DatagramPacket datagramPacket = new DatagramPacket(string, messagePost.length(), machineAddress, portToSend);
+            System.out.println("enviando mensaje a " + portToSend);
+            sSocket.send(datagramPacket);
+            System.out.println("mensaje enviado");
+            sSocket.close();
+        } catch (UnknownHostException e) {
+            System.err.println("Desconocido: " + e.getMessage());
+        } catch (SocketException e) {
+            System.err.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("E/S: " + e.getMessage());
+        }
+    }
+
+    public void sendToken(int port){
+        try {
+            DatagramSocket sSocket = new DatagramSocket();
+            InetAddress machineAddress = InetAddress.getByName("localhost");
+            byte[] string = tokenName.getBytes();
+            DatagramPacket datagramPacket = new DatagramPacket(string, string.length, machineAddress, port);
+            sSocket.send(datagramPacket);
+            sSocket.close();
+        } catch (UnknownHostException e) {
+            System.err.println("Desconocido: " + e.getMessage());
+        } catch (SocketException e) {
+            System.err.println("Socket: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("E/S: " + e.getMessage());
         }
     }
 }
